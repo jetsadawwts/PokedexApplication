68ab5c21af4a7fed2ede034a7707acfbacabcac5
Jetsadawwts <jetsadawwts@gmail.com>
2022-10-15 12:23:43 +0700

    Pokédex List And Pokédex Search

--- app/src/main/java/com/jetsadawwts/pokedexapplication/MainActivity.kt
+++ app/src/main/java/com/jetsadawwts/pokedexapplication/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import com.jetsadawwts.pokedexapplication.pokemonlist.PokemonListScreen
 import com.jetsadawwts.pokedexapplication.ui.theme.PokedexApplicationTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -22,7 +23,7 @@ class MainActivity : ComponentActivity() {
                 val navController = rememberNavController()
                 NavHost(navController = navController, startDestination = "pokemon_list_screen") {
                         composable("pokemon_list_screen") {
-
+                            PokemonListScreen(navController = navController)
                         }
                         composable(
                             "pokemon_list_detail_screen/{dominantColor}/{pokemonName}",
